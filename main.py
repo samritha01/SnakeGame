24dffbcd54422e5c1bb39d6fbfbe7f87b78b4863
samrithaeiffel <sammykite2001@gmail.com>
2020-01-10 04:14:41 +0000

    width and hieght

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import sys
 from pygame.locals import *
 
 Snakespeed = 17
-Window_Width = 890
-Window_Height = 500
+Window_Width = 1000
+Window_Height = 800
 Cell_Size = 20  # Width and height of the cells
 # Ensuring that the cells fit perfectly in the window. eg if cell size was
 # 10     and window width or windowheight were 15 only 1.5 cells would
